7124235562bd9b2b7234a51cc0a3ccfa8e7d2ba9
EDSON BUACHACK PEREIRA <edson.4978@aluno.pr.senac.br>
2024-11-21 19:12:22 -0300

    Início do projeto no git

---
+++ src/test/java/pr/senac/br/MyTest.java
@@ -0,0 +1,11 @@
+package pr.senac.br;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyTest {
+    @Test
+    void exampleTest() {
+        assertTrue(true); // Exemplo simples de teste
+    }
+}
